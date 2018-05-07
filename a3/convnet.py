4e47e2ef6d6af49b4a1491ed43914e1fa9e83798
yonk <nopctoday@gmail.com>
2018-05-07 16:29:33 +0200

    cleaned up code

--- a3/convnet.py
+++ a3/convnet.py
@@ -1,10 +1,9 @@
 import numpy as np
-from scipy.sparse import csr_matrix
-from numpy import tensordot, maximum, sum
+from numpy import sum
 from numpy.random import normal
+import matplotlib.pyplot as plt
 from time import time
-from typing import List
-from scipy.io import loadmat
+
 
 NAMES = 'ascii_names.txt'
 D = 28 # Number of chars
@@ -58,12 +57,12 @@ class ConvNet():
         for i, ki in enumerate(self.hp.k):
             ni = self.hp.nf[i]
             #self.f[i] = normal(0, self.hp.sigma[i], (d, ki, ni))
-            self.f[i] = np.random.randn(d, ki, ni) * 0.01
+            self.f[i] = np.random.randn(d, ki, ni) * 0.1
             d = ni
 
         fsize = self.hp.nf[-1] * self.hp.nlen[-1]
         #self.w = normal(0, self.hp.sigma[-1],(self.hp.K, fsize))
-        self.w = np.random.randn(self.hp.K, fsize) * 0.01
+        self.w = np.random.randn(self.hp.K, fsize) * 0.1
         self.mf=[]
         self.dF = [0] * filter_size
         self.dF2 = 0
@@ -77,7 +76,6 @@ class ConvNet():
     def compute_batch(self, X, Y, W=None):
         batchsize = self.hp.batch_size
         data_size = X.shape[1]
-        print(self.compute_loss(X,Y))
         t0 = time()
         print('begin preprocessing ...')
         self.preprocess_mx_superefficient(X, batchsize)
@@ -95,26 +93,6 @@ class ConvNet():
 
             p = self.forward(x_batch)
             dw, df = self.backward(y_batch, p)
-            # print('begin numerical grad')
-            # ndw, ndf1, ndf2 = self.compute_num_grads_center(x_batch, y_batch)
-            #
-            # df1, df2 = df
-            # diffw = dw-ndw
-            # diff1 = ndf1 - df1
-            # diff2 = ndf2 - df2
-            # diff1_max = np.max(np.abs(diff1))
-            # snd2 = np.abs(ndf1).sum()
-            # sd2 = np.abs(df1).sum()
-            #
-            # print('d1  ',sd2)
-            # print('nd1 ', snd2)
-            # print('diff', snd2 - sd2)
-            # print(diff1.sum())
-            # z = df2
-            # zz =  ndf2
-            # print('done numerical grad')
-            # df = [ndf1, ndf2]
-
 
             self.dW_momentum = self.dW_momentum * self.hp.rho + dw * self.hp.etta
             self.dF_momentum = [ f * self.hp.rho + df[i] for i,f in enumerate(self.dF_momentum)]
@@ -122,10 +100,10 @@ class ConvNet():
             self.w -= self.dW_momentum
             self.f = [ f - self.dF_momentum[i] for i, f in enumerate(self.f)]
 
-            print('loss',self.compute_loss(X, Y))
+            #print('loss',self.compute_loss(X, Y))
 
-        print('epoch time:', time()-t0)
-        print(self.compute_loss(X, Y))
+            print('loss',self.loss(x_batch, y_batch, p=p))
+            print('accuracy', '%0.2f%%' % self.accuracy(x_batch, y_batch, p=p))
 
     def forward(self, x_input, params=None):
         if params is not None:
@@ -163,7 +141,6 @@ class ConvNet():
 
         # Dense layer
         dW = G.dot(x.T) / n
-
         G_ = self.w.T.dot(G)
         ind = x > 0
         G_ = G_ * ind
@@ -178,18 +155,20 @@ class ConvNet():
             f = self.f[i]
             d, k, nf = f.shape
 
-            # use precomputed value
+
             if i == 0:
-                #mx = self.pre_mx1_batch
-                v_vec = self._branch0(G_, n)
-                #v_vec2 = self._branch1(x, d, k, nf, G_, n)
+                # use precomputed value
+                v_vec = np.zeros(self.hp.precomputed_v1_dimension)
+                for col_ix, gix in self.mx1_batch.items():
+                    to_be_summed = G_.take(gix)
+                    v_vec[col_ix] = np.sum(to_be_summed)
+                v_vec /= n
             else:
-                v_vec = self._branch1(x,d,k,nf, G_, n)
+                mx = self.make_mx_matrix(x, d, k, nf, optimized=True)
+                new_g = G_.reshape(mx.shape[0], nf, -1)  # nf should actually be cols, but einsum eliminates the need
+                v_vec = np.einsum('ijk,iyk->jy', mx, new_g) / n
 
             v = v_vec.reshape(f.shape, order='F')
-            # bug fix
-            if i == 0:
-                v = v_vec.reshape(f.shape, order='F')
 
             self.dF[i] = v
             mf = self.mf[i]
@@ -197,128 +176,6 @@ class ConvNet():
             G_ = G_ * (x > 0)
 
         return dW, self.dF
-
-    def _branch0(self, G_, n):
-        v_vec = np.zeros(self.hp.precomputed_v1_dimension)
-        for col_ix, gix in self.mx1_batch.items():
-            to_be_summed = G_.take(gix)
-            v_vec[col_ix] = np.sum(to_be_summed)
-
-        return v_vec / n
-
-    def _branch1(self, x, d, k, nf, G_, n):
-        mx = self.make_mx_matrix(x, d, k, nf)
-        # v_vec = np.einsum('ik,ijk->j', G_, mx) / n
-        #
-        # try slow
-        s_vec = np.zeros(mx.shape[1])
-        for i in range(G_.shape[1]):
-            g = G_[:,i]
-            #g1 = g.reshape(mx.shape[0], nf)
-            m1 = mx[:,:,i]
-            g2 = g.dot(m1)
-            s_vec += g2
-        #s_vec /= n
-
-        # Optmized version
-        mx = self.make_mx_matrix(x, d, k, nf, optimized=True)
-        new_g = G_.reshape(mx.shape[0],nf,-1) # nf should actually be cols, but einsum eliminates the need
-        v_vec = np.einsum('ijk,iyk->jy', mx, new_g)#.flatten('F')  / n # Normalize
-
-        #
-        # #slow way
-        # s_vec = np.zeros((mx.shape[1], nf))
-        # for i in range(G_.shape[1]):
-        #     g = G_[:,i]
-        #     g1 = g.reshape(mx.shape[0], nf)
-        #     m1 = mx[:,:,i]
-        #     g2 = m1.T.dot(g1)
-        #     s_vec += g2
-        # s_vec /= n
-        #
-        # s = np.sum(s_vec) - np.sum(v_vec)
-        # d = s_vec - v_vec
-
-        f = self.f[1]
-        v_vec = v_vec.reshape(f.shape, order='F')
-        s_vec = s_vec.reshape(f.shape, order='F')
-        diff = v_vec - s_vec
-        return s_vec
-
-    def pre_process_mx(self,X, batch_size):
-        d, k, nf = self.f[0].shape
-        precomputed_mx1 = self.make_mx_matrix(X, d, k, nf)
-        mx_rows, mx_cols, _ = precomputed_mx1.shape
-
-        # swapped to: mx_cols(output dim) x mx_rows x data_size
-        swapped = np.swapaxes(precomputed_mx1, 1, 0)
-        data_size = X.shape[1]
-
-        self.precomputed_mx1 = []
-        print('begin loop')
-        t1 = time()
-        # precompute to save time on mod
-        for b_start in np.arange(0, data_size, batch_size):
-            print(b_start/batch_size, time()-t1)
-            b_end = b_start+batch_size if b_start+batch_size < data_size else data_size
-            batch = swapped[:,:, b_start:b_end]
-
-            # second loop actually speeds things up a bit
-            cols_in_batch = []
-            for j in range(mx_cols):
-                ix_2d = np.argwhere(batch[j,:,:] > 0)
-                if ix_2d.size == 0:  #Empty column
-                        continue
-
-                gix = ix_2d[:, 0] * batch_size + ix_2d[:, 1] # g_row * batch_size + batch number
-                cols_in_batch.append((j, gix))
-
-            self.precomputed_mx1.append(cols_in_batch)
-
-    def make_mf_matrix(self, F, nlen):
-        dd, k, nf = F.shape
-        flen = k * dd
-        rows = (nlen - k + 1) * nf
-        columns = nlen * dd
-        F = F.reshape((flen, nf), order='F')
-
-        mf = np.zeros((rows, columns))
-
-        for i in range(rows):
-            idx = int(i / nf) * dd
-            mf[i, idx:idx + flen] = F[:, i % nf]
-        return mf
-
-    def make_mx_matrix(self, x_input, d, k, nf, optimized=False):
-        nlen = int(x_input.shape[0] / d)
-
-        vec_x_cols = d * k
-        x_rows = (nlen - k + 1)
-        rows = x_rows * nf
-        cols = vec_x_cols * nf
-
-        batch_size = x_input.shape[1]
-
-        x_vecs = np.ndarray((x_rows, vec_x_cols, batch_size))
-
-        # stride x to list
-        for i in range(x_rows):
-            idx = i * d
-            x_vecs[i, :] = x_input[idx:idx + vec_x_cols]
-
-        if optimized:
-            return x_vecs
-
-        mx = np.zeros((rows, cols, batch_size))
-
-        # add in into large matrix
-        for i in range(rows):
-            idx = (i % nf) * vec_x_cols
-            r = int(i / nf)
-            deleme = x_vecs[r, :]
-            mx[i, idx:idx + vec_x_cols] = deleme
-
-        return mx
 
     def preprocess_mx_superefficient(self, x_input, batch_size):
 
@@ -374,6 +231,51 @@ class ConvNet():
 
         self.precomputed_mx1_full = batches
 
+    def make_mf_matrix(self, F, nlen):
+        dd, k, nf = F.shape
+        flen = k * dd
+        rows = (nlen - k + 1) * nf
+        columns = nlen * dd
+        F = F.reshape((flen, nf), order='F')
+
+        mf = np.zeros((rows, columns))
+
+        for i in range(rows):
+            idx = int(i / nf) * dd
+            mf[i, idx:idx + flen] = F[:, i % nf]
+        return mf
+
+    def make_mx_matrix(self, x_input, d, k, nf, optimized=False):
+        nlen = int(x_input.shape[0] / d)
+
+        vec_x_cols = d * k
+        x_rows = (nlen - k + 1)
+        rows = x_rows * nf
+        cols = vec_x_cols * nf
+
+        batch_size = x_input.shape[1]
+
+        x_vecs = np.ndarray((x_rows, vec_x_cols, batch_size))
+
+        # stride x to list
+        for i in range(x_rows):
+            idx = i * d
+            x_vecs[i, :] = x_input[idx:idx + vec_x_cols]
+
+        if optimized:
+            return x_vecs
+
+        mx = np.zeros((rows, cols, batch_size))
+
+        # add in into large matrix
+        for i in range(rows):
+            idx = (i % nf) * vec_x_cols
+            r = int(i / nf)
+            deleme = x_vecs[r, :]
+            mx[i, idx:idx + vec_x_cols] = deleme
+
+        return mx
+
     def compute_num_grads_center(self, X, Y, h=1e-5):
         """
         A somewhat slow method to numerically approximate the gradients using the central difference.
@@ -395,9 +297,9 @@ class ConvNet():
                 ix = it.multi_index
                 old_value = param[ix]
                 param[ix] = old_value + h
-                plus_cost = self.compute_loss(X, Y, params)
+                plus_cost = self.loss(X, Y, params)
                 param[ix] = old_value - h
-                minus_cost = self.compute_loss(X, Y, params)
+                minus_cost = self.loss(X, Y, params)
                 param[ix] = old_value  # Restore original value
 
                 grad[ix] = (plus_cost - minus_cost) / (2 * h)
@@ -407,8 +309,9 @@ class ConvNet():
 
         return num_grads
 
-    def compute_loss(self, X, Y, params=None):
-        p = self.forward(X, params=params)
+    def loss(self, X, Y, p=None, params=None):
+        if p is None:
+            p = self.forward(X, params=params)
         batch_count = Y.shape[1]
         # faster sum
         prod = sum(Y * p, axis=0)
@@ -418,10 +321,47 @@ class ConvNet():
         loss_normalized = summed / batch_count
         return loss_normalized
 
+    def accuracy(self, X, Y, p=None, params=None):
+        if p is None:
+            p = self.forward(X, params=params)
+        winners = np.argmax(p, axis=0)  # 2D vector
+        truth = np.argmax(Y, axis=0)
+        diff = truth - winners
+
+        s1 = np.count_nonzero(diff)
+        s2 = Y.shape[1]
+        s3 = s1/s2
+        accuracy = 1 -  s3
+        return accuracy * 100
+
     def check_grad(self, a, n, eps):
-        diff = np.abs(a - n) / np.maximum(eps, np.amax(np.abs(a) + np.abs(n)))
-        if np.amax(diff) < 1e-6:
-            return True
-        else:
-            return False
+        print('begin numerical grad')
+        ndw, ndf1, ndf2 = self.compute_num_grads_center(x_batch, y_batch)
+        df1, df2 = df
+        diffw = dw-ndw
+        diff1 = ndf1 - df1
+        diff2 = ndf2 - df2
+        diff1_max = np.max(np.abs(diff1))
+        snd2 = np.abs(ndf1).sum()
+        sd2 = np.abs(df1).sum()
+        print('F1 cumulative diff', snd2 - sd2)
+        print(diff1.sum())
+        z = df2
+        zz =  ndf2
+        print('done numerical grad')
+
+    def _plot_loss(self, loss, validation_loss=None):
+        plt.figure()
+        plt.plot(loss)
+        plt.title('training loss')
+        plt.xlabel('update cycle')
+        plt.show()
+
+    def _plot_accuracy(self, accuracy, validation_accuracy=None):
+        plt.figure()
+        plt.plot(accuracy)
+        plt.title('prediction accuracy')
+        plt.xlabel('update cycle')
+        plt.show()
+
 
