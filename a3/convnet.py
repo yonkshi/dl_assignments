bde005f604584450bef45469a3e5f5efd11bd8fb
yonk <nopctoday@gmail.com>
2018-05-09 21:10:29 +0200

    working convnet

--- a3/convnet.py
+++ a3/convnet.py
@@ -10,7 +10,7 @@ D = 28 # Number of chars
 N_LEN = 19 # Maximum char for longest word
 
 X = None
-UPDATE_STEPS = 5000
+UPDATE_STEPS = 20000
 BATCH_SAMPLE_SIZE = 73 # hard coded smallest class in training set
 VALIDATION_SAMPLE_SIZE = 500
 
@@ -26,7 +26,7 @@ k: width of filter, not related to
 '''
 
 class HyperParam:
-    def __init__(self, k=[5,3],nf=[20,20], K = 18, fsize=1337, batch_size = 100, learning_rate=0.001, momentum_coeff = 0.9):
+    def __init__(self, k=[10,8,3],nf=[20,20,20], K = 18, batch_size = 100, learning_rate=0.001, momentum_coeff=0.9, momentum_decay=0.999):
         self.k = k # filter width
         self.nf = nf # number of filters
         self.fsize = (N_LEN - k[-1] + 1) * nf[-1]
@@ -36,10 +36,13 @@ class HyperParam:
         self.etta = learning_rate
         self.rho = momentum_coeff
         self.precomputed_v1_dimension = D * k[0] * nf[0]
+        self.hidden_layers = len(nf)-1 #exclude output
+        self.rho_decay = momentum_decay
 
         # compute nlen
         self.nlen = [N_LEN]
-        for i in range(len(nf)): self.nlen += [self.nlen[-1] - k[i] + 1]
+        for i in range(len(nf)):
+            self.nlen += [self.nlen[-1] - k[i] + 1]
 
         # He initialization
         # Input layer, hidden conv layer, and dense layer
@@ -49,33 +52,35 @@ class HyperParam:
         # Speed comparison bottleneck
 
 class ConvNet():
-    def __init__(self, filter_size=2, hyperparam=HyperParam(), ):
+    def __init__(self, hyperparam=HyperParam(), ):
 
         self.hp = hyperparam
-        self.f = [None] * filter_size
+        self.hidden_f = [None] * self.hp.hidden_layers
         self.nlen = [N_LEN]
-        self.filter_size = filter_size
         # initialize filters
-        n1 = self.hp.nf[0]
         d = D
+        self.input_f = np.random.randn(d, self.hp.k[0], self.hp.nf[0]) * 0.1
+        d = self.hp.nf[0]
         for i, ki in enumerate(self.hp.k):
+            if i == 0: # skip input layer
+                continue
             ni = self.hp.nf[i]
-            #self.f[i] = normal(0, self.hp.sigma[i], (d, ki, ni))
-            self.f[i] = np.random.randn(d, ki, ni) * 0.1
+            #self.hidden_f[i-1] = normal(0, self.hp.sigma[i], (d, ki, ni))
+            self.hidden_f[i-1] = np.random.randn(d, ki, ni) * 0.1
             d = ni
 
         fsize = self.hp.nf[-1] * self.hp.nlen[-1]
         #self.w = normal(0, self.hp.sigma[-1],(self.hp.K, fsize))
         self.w = np.random.randn(self.hp.K, fsize) * 0.1
         self.mf=[]
-        self.dF = [0] * filter_size
         self.dF2 = 0
         self.dF1 = 0
         self.dW = 0
 
         # mommentum
         self.dW_momentum = 0
-        self.dF_momentum = [0] * filter_size
+        self.dF_momentum = [0] * self.hp.hidden_layers
+        self.input_dF_momentum = 0
 
     def compute_batch(self, X, Y, validation_idx, W=None):
         # preparing data
@@ -84,8 +89,11 @@ class ConvNet():
         x_train = np.delete(X, validation_idx, axis=1) # split validation_set for
         y_train = np.delete(Y, validation_idx, axis=1)
 
+        # balance factor:
+        balanced_batch_size = np.min(y_train.sum(axis=1)).astype('int')
+
         batchsize = self.hp.batch_size
-        training_size = X.shape[1]
+        training_size = x_train.shape[1]
         t0 = time()
         print('begin preprocessing ...')
         #self.preprocess_mx_superefficient(X, batchsize)
@@ -98,34 +106,32 @@ class ConvNet():
         validation_accuracy = []
         for z in range(UPDATE_STEPS):
             self.mf = []
-            samples = np.random.randint(0, training_size, BATCH_SAMPLE_SIZE)
-            x_batch = X[:, samples]
-            y_batch = Y[:, samples]
+            samples = np.random.randint(0, training_size, balanced_batch_size)
+            x_batch = x_train[:, samples]
+            y_batch = y_train[:, samples]
 
             # Training sampling:
-            train_samples = np.random.randint(0, training_size, VALIDATION_SAMPLE_SIZE)
-            x_val = X[:, train_samples]
-            y_val = Y[:, train_samples]
+            # train_samples = np.random.randint(0, training_size, VALIDATION_SAMPLE_SIZE)
+            # x_val = X[:, train_samples]
+            # y_val = Y[:, train_samples]
 
             #i = int(start / batchsize)
             #self.mx1_batch = self.preprocess_mx_superefficient(x_batch, x_batch.shape[1]) #self.precomputed_mx1_full[i]
             self.mx1_batch = self.sparse_mx1_to_col(samples)
             p = self.forward(x_batch)
-            dw, df = self.backward(y_batch, p)
+            dw, hidden_df, input_df = self.backward(y_batch, p)
 
             self.dW_momentum = self.dW_momentum * self.hp.rho + dw * self.hp.etta
-            self.dF_momentum = [ f * self.hp.rho + df[i] for i,f in enumerate(self.dF_momentum)]
+            self.dF_momentum = [ f * self.hp.rho + hidden_df[i] for i,f in enumerate(self.dF_momentum)]
+            self.input_dF_momentum = self.input_dF_momentum * self.hp.rho + input_df * self.hp.etta
+
+            self.hp.rho *= self.hp.rho_decay #rho decay
 
             self.w -= self.dW_momentum
-            self.f = [ f - self.dF_momentum[i] for i, f in enumerate(self.f)]
+            self.hidden_f = [f - self.dF_momentum[i] for i, f in enumerate(self.hidden_f)]
+            self.input_f -= self.input_dF_momentum
 
-            # Benchmark and reporting stuff
-            loss += [self.loss(x_batch, y_batch, p=p)]
-            accuracy += [self.accuracy(x_batch, y_batch, p=p)]
-
-            validation_loss += [self.loss(x_val, y_val)]
-            validation_accuracy += [self.accuracy(x_val, y_val)]
-
+            # benchmark stuff
             if z % 100 == 0 and z > 0:
                 print('=== %d / %d Complete ===' % (z, UPDATE_STEPS) )
                 print('time:', '%0.2f' % (time()-t0))
@@ -133,6 +139,14 @@ class ConvNet():
                 print('accu:', self.accuracy(x_batch, y_batch, p=p))
                 print('validation loss:', self.loss(x_val_full, y_val_full))
                 print('validation accu:', self.accuracy(x_val_full, y_val_full))
+
+                # Benchmark and reporting stuff
+                loss += [self.loss(x_batch, y_batch, p=p)]
+                accuracy += [self.accuracy(x_batch, y_batch, p=p)]
+
+                validation_loss += [self.loss(x_val_full, y_val_full)]
+                validation_accuracy += [self.accuracy(x_val_full, y_val_full)]
+
                 t0 = time()
 
         # plotting
@@ -145,12 +159,22 @@ class ConvNet():
             filters = [f1, f2]
             modify_self = False
         else:
-            weights, filters = self.w, self.f
+            weights, filters = self.w, self.hidden_f
             modify_self = True #
 
         X = [x_input]
         n_len = N_LEN
-        # Filters
+
+        # input layer
+        x = X[-1]
+        mf = self.make_mf_matrix(self.input_f, n_len)
+        s1 = mf.dot(x)
+        s2 = np.maximum(s1, 0)
+        n_len = int(mf.shape[0] / self.input_f.shape[2])  # (n_len-k+1) * nf / nf
+        X.append(s2)
+        if modify_self: self.mf.append(mf)
+
+        # Hidden Filters
         for f in filters:
             x = X[-1]
             mf = self.make_mf_matrix(f, n_len)
@@ -164,6 +188,7 @@ class ConvNet():
         s = weights.dot(X[-1])
         nom = np.exp(s)
         denom = np.sum(nom, axis=0)
+
         p = nom / denom
         if modify_self: self.x = X
         return p
@@ -178,42 +203,39 @@ class ConvNet():
         G_ = self.w.T.dot(G)
         ind = x > 0
         G_ = G_ * ind
+        df = [None] * self.hp.hidden_layers
+        for i in np.arange(self.hp.hidden_layers)[::-1]: # [::-1] reverses list
 
-        # conv layers
-        i = len(self.dF)
-
-        for dF in reversed(self.dF):
-            i -= 1
-
-            x = self.x[i]
-            f = self.f[i]
+            x = self.x[i+1] # previous input, and offset the input layer
+            f = self.hidden_f[i]
             d, k, nf = f.shape
 
-
-            if i == 0:
-                # use precomputed value
-                v_vec = np.zeros(self.hp.precomputed_v1_dimension)
-                for col_ix, gix in self.mx1_batch.items():
-                    to_be_summed = G_.take(gix)
-                    v_vec[col_ix] = np.sum(to_be_summed)
-                v_vec /= n
-            else:
-                mx = self.make_mx_matrix(x, d, k, nf, optimized=True)
-                new_g = G_.reshape(mx.shape[0], nf, -1)  # nf should actually be cols, but einsum eliminates the need
-                v_vec = np.einsum('ijk,iyk->jy', mx, new_g) / n
+            mx = self.make_mx_matrix(x, d, k, nf, optimized=True)
+            new_g = G_.reshape(mx.shape[0], nf, -1)  # nf should actually be cols, but einsum eliminates the need
+            v_vec = np.einsum('ijk,iyk->jy', mx, new_g) / n
 
             v = v_vec.reshape(f.shape, order='F')
 
-            self.dF[i] = v
-            mf = self.mf[i]
+            #self.dF[i] = v
+            df[i] = v
+            mf = self.mf[i+1] # offset from input layer
             G_ = mf.T.dot(G_)
             G_ = G_ * (x > 0)
 
-        return dW, self.dF
+        # input conv layer
+        # use precomputed value
+        v_vec = np.zeros(self.hp.precomputed_v1_dimension)
+        for col_ix, gix in self.mx1_batch.items():
+            to_be_summed = G_.take(gix)
+            v_vec[col_ix] = np.sum(to_be_summed)
+        v_vec /= n
+        input_df = v_vec.reshape(self.input_f.shape, order='F')
+
+        return dW, df, input_df
 
     def preprocess_mx_superefficient(self, x_input, batch_size):
 
-        d, k, nf = self.f[0].shape
+        d, k, nf = self.input_f.shape
 
         nonzeros = np.argwhere(x_input.T > 0 )
 
@@ -268,7 +290,7 @@ class ConvNet():
         return cols_idx
 
     def preprocess_mx_fast(self, x_input):
-        d, k, nf = self.f[0].shape
+        d, k, nf = self.input_f.shape
 
         nonzeros = np.argwhere(x_input.T > 0)
         # split up into words, one per
@@ -387,7 +409,7 @@ class ConvNet():
         """
         # df/dx ≈ (f(x + h) - f(x - h))/2h according to the central difference formula
 
-        params = [np.copy(self.w), np.copy(self.f[0]), np.copy(self.f[1])]
+        params = [np.copy(self.w), np.copy(self.hidden_f[0]), np.copy(self.hidden_f[1])]
         num_grads = []
 
         for i, param in enumerate(params):
